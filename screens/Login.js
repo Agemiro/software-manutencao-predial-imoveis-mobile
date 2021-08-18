62ecd9239ab28ab62351b66df1eede279441244d
Jordielson <jordielsonemanuel@gmail.com>
2021-08-18 00:47:28 -0300

    mudança de carregando para execuntado no login

--- screens/Login.js
+++ screens/Login.js
@@ -70,7 +70,7 @@ export default function Login({navigation}) {
     <View style={[styles.container, specificStyle.specificContainer]}>
 
       { isLoadingToken && 
-        <Text>Carregando...</Text> 
+        <Text>Executando...</Text> 
       }
 
       { !isLoadingToken && 
